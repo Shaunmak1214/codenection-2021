94368eee35172955db123d605ec5db612d1ef23c
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2021-09-26 13:44:52 +0800

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 function App() {
-  return;
-  <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
