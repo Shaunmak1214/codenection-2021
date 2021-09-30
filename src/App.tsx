cc3b1ca6bc31576b5a5d92143a92efeaa34ee029
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2021-09-30 20:21:46 +0800

    feat: landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { HStack } from '@chakra-ui/layout';
+import Landing from './pages/landing';
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App" style={{ background: '#002A97' }}>
+            <Landing />
+            <HStack h="80vh" bg="#FFFFFF"></HStack>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/atoms/Buttons/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Text } from '@chakra-ui/react';
+
+const PrimaryButton = ({ ...props }) => {
+    let children: React.ReactNode = props.children;
+    return (
+        <Button
+            borderRadius="0"
+            border="2px solid #FFFFFF"
+            bg="#002A97"
+            color="#FFFFFF"
+            _hover={{
+                bg: '#FFFFFF',
+                color: '#002A97',
+            }}
+            {...props}
+        >
+            <Text>{children}</Text>
+        </Button>
+    );
+};
+
+PrimaryButton.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export { PrimaryButton };
