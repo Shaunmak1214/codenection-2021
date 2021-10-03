62757c503cf7b38385242f6f958cf01bc6859049
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2021-10-03 15:59:14 +0800

    feat/sponsorship

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,5 +1,14 @@
-import { PrimaryButton, SecondaryButton } from './Buttons';
+import { PrimaryButton, SecondaryButton, HeaderButton } from './Buttons';
 import CNSpacer from './CNSpacer';
 import { PrimaryText } from './Text';
+import { BadgeImg, SponsorImg } from './SponsorAtom';
 
-export { PrimaryButton, SecondaryButton, CNSpacer, PrimaryText };
+export {
+    PrimaryButton,
+    SecondaryButton,
+    CNSpacer,
+    PrimaryText,
+    HeaderButton,
+    BadgeImg,
+    SponsorImg,
+};
