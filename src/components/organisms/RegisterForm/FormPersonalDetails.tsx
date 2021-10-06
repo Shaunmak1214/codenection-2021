ef4866db070fc2b22ad68bd3fcecc9e0b3dee500
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-10-07 03:04:45 +0800

    framer motion, dynamic svg color

--- src/components/organisms/RegisterForm/FormPersonalDetails.tsx
+++ src/components/organisms/RegisterForm/FormPersonalDetails.tsx
@@ -1,20 +1,117 @@
+/* eslint-disabled */
+// @ts-nocheck
+
 import React from 'react';
-import { PrimaryButton, SecondaryButton } from '../../atoms';
+import { PrimaryButton } from '../../atoms';
+
+import { Center, VStack, Container, HStack } from '@chakra-ui/layout';
+import { Text } from '@chakra-ui/react';
+import { Formik, Form, Field } from 'formik';
+import { CNTextFormField, SecondaryText } from '../../atoms';
+import { EmailIcon, PasswordIcon } from '../../../assets';
+import * as yup from 'yup';
+import { motion } from 'framer-motion';
 
 interface Props {
   nextStep: () => void;
   prevStep: () => void;
 }
 
+interface MyFormValues {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const FormPersonalDetails = ({ nextStep, prevStep }: Props) => {
+  const continueNext = (e: any) => {
+    e.preventDefault();
+    nextStep();
+  };
+
+  const schema = yup.object({
+    email: yup.string().required(),
+    password: yup.string().required(),
+    confirmPassword: yup.string().required(),
+  });
+  const initialValues: MyFormValues = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
   return (
-    <>
-      <div>
-        <h1>Form Personal Details</h1>
-      </div>
-      <SecondaryButton onClick={nextStep}>Next</SecondaryButton>
-      <PrimaryButton onClick={prevStep}>Back</PrimaryButton>
-    </>
+    <Center h="100vh" w="50%">
+      <motion.div
+        initial={{ opacity: 0, x: 75 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Container w="550px" maxW="container.form">
+          <Container>
+            <Text color="#5B5B5B">Step 2</Text>
+            <SecondaryText fontWeight="bold" fontSize="4xl">
+              Build your profile
+            </SecondaryText>
+            <Text color="#5B5B5B">
+              Setting up a profile to represent yourself.
+            </Text>
+          </Container>
+
+          <Container w="100%" py="30px">
+            <Formik
+              validationSchema={schema}
+              initialValues={initialValues}
+              onSubmit={() => console.log('submitted')}
+            >
+              {() => (
+                <Form>
+                  <VStack>
+                    <Field
+                      label="Student Email: "
+                      name="email"
+                      leftIcon={EmailIcon}
+                      component={CNTextFormField}
+                    />
+                    <Field
+                      label="Password: "
+                      name="password"
+                      leftIcon={PasswordIcon}
+                      component={CNTextFormField}
+                      type="password"
+                    />
+                    <Field
+                      label="Confirmation Password: "
+                      name="confirmPassword"
+                      leftIcon={PasswordIcon}
+                      component={CNTextFormField}
+                      type="password"
+                    />
+                  </VStack>
+                  <HStack mt="35px">
+                    <PrimaryButton
+                      borderRadius="8px"
+                      w="100%"
+                      _hover={{ bg: '#000000' }}
+                      onClick={prevStep}
+                    >
+                      Back
+                    </PrimaryButton>
+                    <PrimaryButton
+                      borderRadius="8px"
+                      w="100%"
+                      _hover={{ bg: '#000000' }}
+                      onClick={continueNext}
+                    >
+                      Next
+                    </PrimaryButton>
+                  </HStack>
+                </Form>
+              )}
+            </Formik>
+          </Container>
+        </Container>
+      </motion.div>
+    </Center>
   );
 };
 
