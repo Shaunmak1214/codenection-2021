40915c7b57f7ef1f7bd63c700e42312264004880
Hester <wenxuan1031838@gmail.com>
2021-10-12 00:13:16 +0800

    feat/faq

---
+++ src/components/organisms/FaqSection/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Center, Container } from '@chakra-ui/layout';
+
+import { SecondaryText } from '../../atoms';
+import FaqBlock from '../../molecules/FaqBlock';
+
+const FaqSection = () => {
+  return (
+    <Center py="100px">
+      <Container maxW="container.xl">
+        <SecondaryText
+          fontWeight="bold"
+          fontSize="3xl"
+          mb="40px"
+          textAlign="center"
+        >
+          FAQ
+        </SecondaryText>
+        <Box justifyContent="center" px="35">
+          <FaqBlock />
+        </Box>
+      </Container>
+    </Center>
+  );
+};
+
+export default FaqSection;
