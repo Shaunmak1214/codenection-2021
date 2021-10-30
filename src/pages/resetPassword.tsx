cddca85c6b1e031c19e4a0d9eba60108dc743cc4
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-10-31 05:52:22 +0800

    fix: reset password form responsivess

--- src/pages/resetPassword.tsx
+++ src/pages/resetPassword.tsx
@@ -3,24 +3,32 @@ import { ResetPasswordForm } from '../components/organisms';
 import { HStack, VStack, Center, Container } from '@chakra-ui/layout';
 import { Image } from '@chakra-ui/react';
 import { PrimaryText } from '../components/atoms';
+
+import { useWindowSize } from '../hooks';
+
 const ResetPassword = () => {
+  // eslint-disable-next-line
+  const [windowWidth, windowHeight] = useWindowSize();
+
   return (
-    <HStack alignItems="flex-start">
+    <HStack id="non-scrollable" alignItems="flex-start">
       <ResetPasswordForm />
-      <Center h="100vh" w="50%" bg="#002A97" position="sticky" top="0px">
-        <Container maxW="container.xl">
-          <VStack>
-            <Image />
-            <PrimaryText fontSize="30px" fontWeight="bold">
-              Compete & Connect.
-            </PrimaryText>
-            <PrimaryText fontSize="15px">
-              Compete and connect with teams from all
-            </PrimaryText>
-            <PrimaryText fontSize="15px">malaysia universities.</PrimaryText>
-          </VStack>
-        </Container>
-      </Center>
+      {windowWidth > 1120 && (
+        <Center h="100vh" w="50%" bg="#002A97" position="sticky" top="0px">
+          <Container maxW="container.xl">
+            <VStack>
+              <Image />
+              <PrimaryText fontSize="30px" fontWeight="bold">
+                Compete & Connect.
+              </PrimaryText>
+              <PrimaryText fontSize="15px">
+                Compete and connect with teams from all
+              </PrimaryText>
+              <PrimaryText fontSize="15px">malaysia universities.</PrimaryText>
+            </VStack>
+          </Container>
+        </Center>
+      )}
     </HStack>
   );
 };
