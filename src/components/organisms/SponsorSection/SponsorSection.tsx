fbcca7873b5164e923852cf095e63931fa24b0a0
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-11-01 12:11:24 +0800

    feat: sponsorship proposal

--- src/components/organisms/SponsorSection/SponsorSection.tsx
+++ src/components/organisms/SponsorSection/SponsorSection.tsx
@@ -10,7 +10,11 @@ import {
   BronzeSponsor,
 } from '../../../data/sponsorData';
 import { SecondaryButton } from '../../atoms';
-import { BecomeOurSponsorIcon, StockSponsor } from '../../../assets';
+import {
+  BecomeOurSponsorIcon,
+  SponsorProposal,
+  StockSponsor,
+} from '../../../assets';
 import { Image } from '@chakra-ui/image';
 const SponsorSection = () => {
   const sponsorVisible = false;
@@ -44,7 +48,10 @@ const SponsorSection = () => {
                 py="25px"
                 borderRadius="15px"
                 onClick={() => {
-                  window.location.href = 'mailto:itsoc.mmu@gmail.com';
+                  window.open(
+                    'https://00f74ba44b8d608dcb2724e664b4ca224c384745ac-apidata.googleusercontent.com/download/storage/v1/b/codenection-resumes/o/sponsorship_proposal.pdf?jk=AFshE3WZ-xJ898_Dfnse1-ia1c-IxvORJKeIkInFdxnaHc50Hac2bhEze-0CnoKo-qspqnFROn8wRkGQmyMJdE_DrtuJ9cxXIi6eUZ1jWRiCsXIcA67Qtet94Kt1mItFuuVEcaLoHsfkwuPJEkjB74OYYGl3VV2092sjfA8ev6JYMGwNRjsEbgtU-AGl0432X9CQWtWrwy8E2-_qH3aplI2Zl0i_T5ywVXqqjfNknX3QAJMAOljGiGT8vH1u8YV8h-Y5oisveHfc6t5JvZyuXiOgO4pdMK27lhW1KOQ-ByBg4UUDFFT1i0AFq41WEMbsPVo1-4Dzwm3WvUGlD7r4PTALJfW89NiIF2E38WjXTlgkatPXNthNjJhZj5NzbOCDtFrTg9TyHM11D_sCrOmfRAYIvp4AT23iaTk2ZyWGQLHORppKuGonevpkodZtAfn5lEzmhC46AUNDF6suJbNyx8aXIR-yPGF2_iNSozb7IkqCyFws6XeN3HDnUS17shUxWa9Bfkk_lCaRhToiyznlF_HFiRy18UrYhpRczL0-qBmVkavyu5yxZj16-tRu0x4NuGFseS3DzKbJz0hG-3sjzW00UIl583GsObFGYvtJZl9i0vCek2pU3iK5Tmn8XGyn4jBJXZPtJek88l9H6HNoxQRVPX-138AwgeuCm4Po5QaMmL5_Xc4XUYxwIulJEHjLvCFKfGBJo9aEZd6iY1N-gXeHpg9imSlt9pSOF5SRxsAT50Sri-aW2gWEOfDhxOEAQQ_Xe4-fZB25zIAWPbpGR2aACVGGxLx1VT757oogbjZuxc4DCvmgdqUWB5wGxcdGTV6Q8gG-bFplO2glWLwEBhRX2-7ZRnF3fYuv5teiSNv_bWu4P39A_w3EntCMg1GazstO5Yog2Zgz3FamzKB02FdI3K6Z7MfmPtuOD-8YVKwjdC86o6Clfy1r9x3ckvQMOZwJbiuzet8uZd37yrZax9fk&isca=1',
+                    '_blank',
+                  );
                 }}
               >
                 <HStack alignItems="center">
@@ -100,7 +107,7 @@ const SponsorSection = () => {
               py="25px"
               borderRadius="15px"
               onClick={() => {
-                window.location.href = 'mailto:itsoc.mmu@gmail.com';
+                window.open(SponsorProposal, '_blank');
               }}
             >
               <HStack alignItems="center">
