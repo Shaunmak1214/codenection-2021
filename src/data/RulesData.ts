f2e191ab71f38c6bb6dac09ab7603ad6dd761d52
sinyin-ong <69614329+sinyin-ong@users.noreply.github.com>
2021-11-06 13:35:44 +0800

    fix: rules updates, add partner, chg footer logo

--- src/data/RulesData.ts
+++ src/data/RulesData.ts
@@ -7,13 +7,13 @@ interface RulesDetails {
 const RulesData: RulesDetails[] = [
   {
     idx: 0,
-    title: 'Registration Details',
-    desc: 'CodeNection 2021 Hackathon will be opened for registration on 31 October, 2021 (dates are subject to change at the discretion of the organizers)',
+    title: 'Team Formation Rules',
+    desc: 'Participants are allowed to participate Open Category Contest as long as they are currently pursuing education in any Malaysian university/college, regardless of nationality.',
   },
   {
     idx: 1,
-    title: 'Registration Details',
-    desc: 'Teams that have successfully registered for the event will be sent a confirmation email within 2 weeks.',
+    title: 'General Rules',
+    desc: 'Only the team leader will be required to register for the competition on HackerRank. It is mandatory that the team leader joins the competition using the same username that was submitted during registration.',
   },
   {
     idx: 2,
@@ -22,18 +22,18 @@ const RulesData: RulesDetails[] = [
   },
   {
     idx: 3,
-    title: 'Team Formation Rules',
-    desc: 'Any teams that register for the competition after the deadline will not be allowed to participate in this competition.',
+    title: 'Registration Details',
+    desc: 'Teams that have successfully registered for the event will be sent a confirmation email within 2 weeks.',
   },
   {
     idx: 4,
     title: 'Team Formation Rules',
-    desc: 'All three (3) members can either be from the same faculty, or a mixture of students from multiple faculties.',
+    desc: 'A given participant may only be a part of one (1) team throughout the whole duration of both the Closed and Open Category competitions.',
   },
   {
     idx: 5,
     title: 'Team Formation Rules',
-    desc: 'Participants must not currently be a member of CodeNection’s committee or be a volunteer of this event.',
+    desc: 'Any teams that register for the competition after the deadline will not be allowed to participate in this competition.',
   },
 ];
 
