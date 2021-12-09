371bca8739e78ee6e867a4cd0a530df23eb48593
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-12-09 22:37:18 +0800

    feat: register route block

---
+++ push.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+ 
+while getopts ":p:" opt; do
+  case $opt in
+    p)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
